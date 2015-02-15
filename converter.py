de09c2fdc1c73eab11d09fcafdcaabedfa115a57
bruce li <lixiangdong@dhgate.com>
2015-02-15 19:11:39 +0800

    add more content

--- converter.py
+++ converter.py
@@ -8,6 +8,13 @@ SEGUE_RELATIONSHIP_ROOT = "rootViewController"
 EXPRESS_TAG = "[[ROOT_CONTROLLER]]"
 OC_SWIFT = "swift"
 
+#custom controller
+CUSTOM_CONTROLLER_TAG = "[[CUSTOM_CLASS]]"
+CONTROLLER_OUTLETS_TAG = "[[OUTLETS]]"
+CONTROLLER_SUBVIEWS_TAG = "[[SUBVIEWS]]"
+CONTROLLER_ACTIONS_TAG = "[[ACTIONS]]"
+SUBVIEW_TYPE_LIST = ["button", "label", "textfield", "textview", "tableview", "imageview"]
+ 
 tree = ET.parse("material/storyboard.xml")
 root = tree.getroot()
 
@@ -58,6 +65,7 @@ if initController.tag == "navigationController":
                     customClass = relationshipControllers[0].attrib["customClass"]
                     if OC_SWIFT == "swift":
                         # here's swift stuff
+                        # app delegate
                         controller1 = "controller1 = %s()" % (customClass)
                         root_controller = "var nav = UINavigationController(rootViewController: controller1)\n self.window?.rootViewController = nav"
                         insert_state = "%s\n%s" %(controller1, root_controller)
@@ -65,8 +73,67 @@ if initController.tag == "navigationController":
                         
                         destFile = file('generated/appDelegate.swift', 'w+')
                         destFile.write(templateText)
+                        
+                        # other controllers
+                        controllerTemplateHandler = open("templates/ViewController.template.swift")
+                        controllerTemplateText = controllerTemplateHandler.read()
+                        controllerTemplateText = controllerTemplateText.replace(CUSTOM_CONTROLLER_TAG, customClass)
+                        
+                        customControllerEl = relationshipControllers[0]
+                        subViewEls = customControllerEl.findall(".//view[@key='view']")
+                        
+                        if len(subViewEls) <= 0:
+                            print "no self.view element"
+                        else:
+                            #find other subviews
+                            subviewEls = customControllerEl.findall(".//subviews/")
+                            print "sub view tag " + subviewEls[0].tag
+                            
+                            for sEl in subviewEls:
+                                #if subview is button
+                                if sEl.tag == "button":
+                                    buttonName = sEl.attrib["id"].replace("-", "")
+                                    print "button name " + buttonName
+                                    
+                                    outlet1 = 'var %s = UIButton! \n' % (buttonName)
+                                    subview1 = "" # if there's value for frame, this will set some value
+                                    
+                                    rectEls = sEl.findall(".//rect[@key='frame']")
+                                    if len(rectEls) <= 0:
+                                        print "no rect"
+                                    else:
+                                        rectEl = rectEls[0]
+                                        rectAttr = rectEl.attrib
+                                        subview1 = '%s.frame = CGRectMake(%s, %s, %s, %s) \n' % (buttonName, rectAttr["x"], rectAttr["y"],\
+                                         rectAttr["width"], rectAttr["height"])
+                                        
+                                    stateEls = sEl.findall(".//state[@title]")
+                                    if len(stateEls) <= 0:
+                                        print "no state which is used on title value"
+                                    else:
+                                        stateAttr = stateEls[0].attrib
+                                        subview1 = subview1 + ('%s.setTitle("%s", forState: %s)\n' % (buttonName, \
+                                        stateAttr["title"], "UIControlState.Normal"))
+                                        
+                                    # insert outlet    
+                                    outletPos = controllerTemplateText.find(CONTROLLER_OUTLETS_TAG)
+                                    if outletPos < 0:
+                                        print "no outlet position placeholder"
+                                    else:
+                                        controllerTemplateText = controllerTemplateText[:outletPos] + outlet1 + controllerTemplateText[outletPos:]
+                                        
+                                    # insert subviews in viewDidLoad
+                                    subviewPos = controllerTemplateText.find(CONTROLLER_SUBVIEWS_TAG)
+                                    if subviewPos < 0:
+                                        print "no subview position placeholder"
+                                    else:
+                                        controllerTemplateText = controllerTemplateText[:subviewPos] + subview1 + controllerTemplateText[subviewPos:]
+                                    
+                                    #TODO: replace placeholders when set all stuff right
+                                    
+                        print "custom controller: " + controllerTemplateText
                     else:
-                        pass
+                        print "your try to generate OC code, which is not implemented yet."
                         # here's oc stuff
             else:
                 print "kind is not relationship. not a relationship segue."
